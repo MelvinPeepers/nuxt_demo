0ffc256488f3e5858fd7a88bf119540ac05d47cc
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2021-04-29 14:51:51 -0400

    Added axios for fetching api data

--- pages/Jokes/index.vue
+++ pages/Jokes/index.vue
@@ -1,15 +1,41 @@
 <template>
     <div>
-        <h1>Jokes</h1>
-        <p>Page for jokes</p>
+        <Joke v-for="joke in jokes" :key="joke.id" :id="joke.id" :joke="joke.joke"  />
     </div>
 </template>
 
 <script>
+import axios from "axios";
+import Joke from '../../components/Joke'
+
 export default {
+    components: {
+        Joke
+    },
+    data() {
+        return {
+            jokes: []
+        }
+    },
+    async created() {
+        const config = {
+            headers: {
+                'Accept': 'application/json'
+            }
+        }
+    
+    try {
+        const res = await axios.get('https://icanhazdadjoke.com/search', config);
+
+        this.jokes = res.data.results;
+    } catch (err) {
+        console.log(err)
+    }
+        
+    },
     head() {
         return {
-            title: 'Jokes',
+            title: 'Corny Dad Jokes',
             meta: [
                 {
                     hid: 'description',
